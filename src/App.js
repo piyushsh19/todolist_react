2835013181ac85f8932ba9f6245d14c1c29feb24
piyush-sh19 <piyush.sh19@gmail.com>
2020-07-20 01:55:44 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const[todos, setTodos] = useState(['this 1', 'second']);
+  const[input, setInput] = useState('');
+  console.log('hi', input);
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    console.log('ho gya');
+    setTodos([...todos, input]);
+    setInput('');
+    
+  }
+
   return (
     <div className="App">
+      
+      <form>
       <h1>Todo-List</h1>
-      <input />
-      <button>Add item</button>
+      <input value = {input} onChange = { event => setInput(event.target.value)}/>
+      <button type="submit" onClick={addTodo}>Add item</button>  
+      </form>
+      
+
+      <ul>
+        {todos.map(todo => (
+          <li>{todo}</li>
+        ))}
+      </ul>
+
     </div>
   );
 }

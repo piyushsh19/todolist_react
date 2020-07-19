fce4751ea815f82b817a7a2211125cde75ef8125
piyush-sh19 <piyush.sh19@gmail.com>
2020-07-20 04:46:12 +0530

    update

--- src/Todo.js
+++ src/Todo.js
@@ -7,7 +7,7 @@ function Todo(props) {
         <List>
             <ListItem>
                 
-                <ListItemText primary= 'Task' secondary= {props.text.text} />
+                <ListItemText primary= 'Task' secondary= {props.todo.todo} />
             </ListItem>
             <Button onClick= {event => {
             db.collection('todos').doc(props.text.id).delete();
